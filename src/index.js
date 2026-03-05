396baeac5f7c6cb5bf0ecc0cd90c9b535c599bbd
Mario Lellis <mariolellis@gmail.com>
2026-03-05 20:20:57 -0300

    fix: add trust proxy for session cookies behind reverse proxy

--- src/index.js
+++ src/index.js
@@ -27,6 +27,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// ---------- Trust Proxy (EasyPanel / Traefik) ----------
+app.set('trust proxy', 1);
+
 // ---------- Session ----------
 app.use(session({
     store: new PgSession({
